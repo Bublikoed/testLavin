cb840c27e2bab2fe323a38b38105cb4aeb6c457a
Vls <bestblodd@gmail.com>
2021-09-05 17:07:27 +0300

    final version

--- src/index.js
+++ src/index.js
@@ -5,18 +5,23 @@ import './assets/css/main.scss';
 //BLOCKS 
 
 import './assets/css/blocks/btn.scss';
+import './assets/css/blocks/inputs.scss';
 import './assets/css/blocks/mobile-menu.scss';
 import './assets/css/blocks/popup.scss';
 import './assets/css/blocks/header.scss';
 import './assets/css/blocks/footer.scss';
 import './assets/css/blocks/main-screen.scss';
+import './assets/css/blocks/lenguage.scss';
+import './assets/css/blocks/form.scss';
+
+
 
 
 
 // JS
-
 import './assets/js/popup';
 import './assets/js/inputs';
 import './assets/js/header';
 import './assets/js/mobile-menu';
 import './assets/js/anchors';
+import './assets/js/animate';
